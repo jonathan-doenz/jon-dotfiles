f7e8443dd6f5899d865966012b23ec45f60e4192
jonathan-doenz <jonathan.doenz@gmail.com>
2018-05-21 05:46:56 +0200

    Add .vimrc, improve linking.sh and documentation

---
+++ README.md
@@ -0,0 +1,12 @@
+# This repository contains the files necessary to implement my terminal and vim preferences on a Unix based system
+
+Follow these steps to get my preferences on your computer.
+
+1) Clone the repository in a folder, for example ~/.dotfiles
+```
+git clone https://github.com/jonathan-doenz/jon-dotfiles ~/.dotfiles/
+```
+2) Run the linking.sh script:
+```
+./~/.linking.sh
+```
--- linking.sh
+++ linking.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Go inside the local repository and pull the latest version of dotfiles directory
+THIS_DIR="$(dirname "${BASH_SOURCE}")";
+cd $THIS_DIR
+#cd ~/.dotfiles/;
+git pull origin master;
+
 # Link all dotfiles to the standard home path ~
-ln -sv ~/.dotfiles/.bash_profile ~
-ln -sv ~/.dotfiles/.bashrc ~
-ln -sv ~/.dotfiles/.bash_prompt ~
-ln -sv ~/.dotfiles/.aliases ~
-ln -sv ~/.dotfiles/.exports ~
-ln -sv ~/.dotfiles/.functions ~
-ln -sv ~/.dotfiles/.path ~
+ln -sv $THIS_DIR/.bash_profile ~;
+ln -sv $THIS_DIR/.bashrc ~;
+ln -sv $THIS_DIR/.bash_prompt ~;
+ln -sv $THIS_DIR/.aliases ~;
+ln -sv $THIS_DIR/.exports ~;
+ln -sv $THIS_DIR/.functions ~;
+ln -sv $THIS_DIR/.path ~;
+ln -sv $THIS_DIR/.vimrc ~;
 
 # Source all the files
-source ~/.bashrc
+source ~/.bashrc;
