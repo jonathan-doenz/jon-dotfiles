766350f6c1affc58ed6aa902d10a49032a283ca6
jonathan-doenz <jonathan.doenz@gmail.com>
2018-05-25 06:46:24 +0200

    Testing syncking script

--- dotfiles_syncker.sh
+++ dotfiles_syncker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-dotfiles=$(find ~/jon-dotfiles/ -name "\.[a-z,A-Z]*")
+dotfiles=("aliases" "bash_profile" "bash_prompt" "bashrc" "exports" "functions" "path") 
+#dotfiles=$(find ~/jon-dotfiles/ -name "\.[a-z,A-Z]*")
 dir="${HOME}/jon-dotfiles"
 
 # Link the files in dotfiles to be the effective dotfiles in home directory
 for dotfile in "${dotfiles[@]}";do
-	#ln -sf "${HOME}/${dotfile}" "${dir}"
-	ln -sf "${dotfile}" "${dir}"
+	ln -sf "${HOME}/${dotfile}" "${dir}"
+	#ln -sf "${dotfile}" "${dir}"
 done
