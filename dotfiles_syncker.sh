c3cb683b6b07c1ed95423a63a0fef72b0307023a
jonathan-doenz <jonathan.doenz@gmail.com>
2018-05-30 04:47:58 +0200

    dotfiles_syncker.sh is now directory independent

--- dotfiles_syncker.sh
+++ dotfiles_syncker.sh
@@ -2,9 +2,9 @@
 
 dotfiles=(".aliases" ".bash_profile" ".bash_prompt" ".bashrc" ".exports" ".functions" ".path" ".vimrc") 
 #dotfiles=$(find ~/jon-dotfiles/ -name "\.[a-z,A-Z]*")
-dir="${HOME}/jon-dotfiles"
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Link the files in dotfiles to be the effective dotfiles in home directory
+# Link the files in dotfiles to be the effective dotfiles normally present in home directory
 for dotfile in "${dotfiles[@]}";do
 	ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}" 
 done
