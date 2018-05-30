ac7740e5c0f8e59c112cd08f971147fa7672bc45
jonathan-doenz <jonathan.doenz@gmail.com>
2018-05-30 05:34:38 +0200

    dotfiles_syncker.sh finds automatically all the dotfiles present in the repo to link them

--- dotfiles_syncker.sh
+++ dotfiles_syncker.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-dotfiles=(".aliases" ".bash_profile" ".bash_prompt" ".bashrc" ".exports" ".functions" ".path" ".vimrc") 
-#dotfiles=$(find ~/jon-dotfiles/ -name "\.[a-z,A-Z]*")
+# Get the directory in which the present file is
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Link the files in dotfiles to be the effective dotfiles normally present in home directory
+# Uncomment this line if you want to manually name the dotfiles you want to link
+#dotfiles=(".aliases" ".bash_profile" ".bash_prompt" ".bashrc" ".exports" ".functions" ".path" ".vimrc") 
+
+# Uncomment this line if you want all the dotfiles in the present directory to be linked except the .git files
+dotfiles=$(find ${dir} -name "\.[a-z,A-Z]*" | grep -v ".git")
+
+# Link the selected dotfiles to be the effective dotfiles in home directory
 for dotfile in "${dotfiles[@]}";do
 	ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}" 
 done
